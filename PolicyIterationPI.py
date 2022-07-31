0a5e72d4a26772754cfa9ea9359d4a6ea7ff960d
sepideh <sepideh.shamsizadeh@gmail.com>
2022-07-31 18:07:10 +0200

    fix bugs of policy_iteration function

--- PolicyIterationPI.py
+++ PolicyIterationPI.py
@@ -2,6 +2,7 @@ import numpy as np
 
 
 def iterative_policy_evaluation(p_state, V, theta, action, rewards, landa):
+    V = np.zeros((4, 4))
     delta = 1
     while delta > theta:
         delta = 0
@@ -15,22 +16,22 @@ def iterative_policy_evaluation(p_state, V, theta, action, rewards, landa):
                     else:
                         if s == 'up':
                             if i - 1 >= 0:
-                                V[i][j] = pp[0] * (rewards[i][j] + landa * V1[i - 1][j])
+                                V[i][j] = pp[0] * (rewards[i - 1][j] + landa * V1[i - 1][j])
                             else:
                                 V[i][j] = pp[0] * (rewards[i][j] + landa * V1[i][j])
                         if s == 'down':
                             if i + 1 <= 3:
-                                V[i][j] = pp[1] * (rewards[i][j] + landa * V1[i + 1][j]) + V[i][j]
+                                V[i][j] = pp[1] * (rewards[i + 1][j] + landa * V1[i + 1][j]) + V[i][j]
                             else:
                                 V[i][j] = pp[1] * (rewards[i][j] + landa * V1[i][j]) + V[i][j]
                         if s == 'left':
                             if j - 1 >= 0:
-                                V[i][j] = pp[2] * (rewards[i][j] + landa * V1[i][j - 1]) + V[i][j]
+                                V[i][j] = pp[2] * (rewards[i][j - 1] + landa * V1[i][j - 1]) + V[i][j]
                             else:
                                 V[i][j] = pp[2] * (rewards[i][j] + landa * V1[i][j]) + V[i][j]
                         if s == 'right':
                             if j + 1 <= 3:
-                                V[i][j] = pp[3] * (rewards[i][j] + landa * V1[i][j + 1]) + V[i][j]
+                                V[i][j] = pp[3] * (rewards[i][j + 1] + landa * V1[i][j + 1]) + V[i][j]
                             else:
                                 V[i][j] = pp[3] * (rewards[i][j] + landa * V1[i][j]) + V[i][j]
                 delta = max(delta, np.abs(V[i][j] - V1[i][j]))
@@ -39,10 +40,11 @@ def iterative_policy_evaluation(p_state, V, theta, action, rewards, landa):
 
 def equalit_marices(a, b):
     eq = True
-    for i in a:
-        for j in b:
-            if i != j:
-                return False
+    for i in range(0, 4):
+        for j in range(0, 4):
+            for k in range(0, 4):
+                if a[i][j][k] != b[i][j][k]:
+                    return False
     return eq
 
 
@@ -51,61 +53,54 @@ def policy_iteration(p_state, Vpi, theta, action, rewards, landa):
     a = 0
     while policy_notstable:
         Vpi = iterative_policy_evaluation(p_state, Vpi, theta, action, rewards, landa)
+        old_action = np.copy(p_state)
         for i in range(0, 4):
             for j in range(0, 4):
-                old_action = p_state[i][j]
+                mm = []
                 for s in action:
                     if i == 0 and j == 0:
                         Vpi[i][j] = 0
+                        mm.append(0)
                     else:
                         if s == 'up':
                             if i - 1 >= 0:
-                                max = Vpi[i - 1][j]
-                                a = 0
+                                x = (rewards[i - 1][j] + landa * Vpi[i - 1][j])
+                                mm.append(x)
                             else:
-                                max = Vpi[i][j]
-                                a = 0
+                                mm.append(-1000)
                         if s == 'down':
                             if i + 1 <= 3:
-                                if Vpi[i + 1][j] > max:
-                                    max = Vpi[i + 1][j]
-                                    a = 1
+                                x = (rewards[i + 1][j] + landa * Vpi[i + 1][j])
+                                mm.append(x)
                             else:
-                                if Vpi[i][j] > max:
-                                    max = Vpi[i][j]
-                                    a = 1
+                                mm.append(-1000)
                         if s == 'left':
                             if j - 1 >= 0:
-                                if Vpi[i][j - 1] > max:
-                                    max = Vpi[i][j - 1]
-                                    a = 2
+                                x = (rewards[i][j - 1] + landa * Vpi[i][j - 1])
+                                mm.append(x)
                             else:
-                                if Vpi[i][j] > max:
-                                    max = Vpi[i][j]
-                                    a = 2
+                                mm.append(-1000)
                         if s == 'right':
                             if j + 1 <= 3:
-                                if Vpi[i][j + 1] > max:
-                                    max = Vpi[i][j + 1]
-                                    a = 3
+                                x = (rewards[i][j + 1] + landa * Vpi[i][j + 1])
+                                mm.append(x)
                             else:
-                                if Vpi[i][j] > max:
-                                    max = Vpi[i][j]
-                                    a = 3
-
+                                mm.append(-1000)
+                a = np.argmax(mm)
                 p = np.zeros(4)
                 if i == 0 and j == 0:
                     p[a] = 0
                 else:
                     p[a] = 1
                 p_state[i][j] = p
-                if equalit_marices(old_action, p_state[i][j]):
-                    policy_notstable = False
+        if equalit_marices(old_action, p_state):
+            policy_notstable = False
+
     return p_state, Vpi
 
 
 if __name__ == '__main__':
-    Vv = np.zeros((4, 4))
+    V = np.zeros((4, 4))
     actions = ['up', 'down', 'left', 'right']
     p = [0.25, 0.25, 0.25, 0.25]
     pi = [[p, p, p, p], [p, p, p, p], [p, p, p, p], [p, p, p, p]]
@@ -113,4 +108,13 @@ if __name__ == '__main__':
     rewards = -1 * np.ones((4, 4))
     rewards[0][1] = rewards[0][2] = rewards[0][3] = -10
     rewards[2][0] = rewards[2][1] = rewards[2][2] = -10
-    policy_iteration(pi, Vv, theta, actions, rewards, 1)
+    ppp, x = policy_iteration(pi, V, theta, actions, rewards, 1)
+    pi_states = [['0', '', '', ''], ['', '', '', ''], ['', '', '', ''],['', '', '', '']]
+    for k in range(0, 4):
+        for l in range(0, 4):
+            if l != 0 or k != 0:
+                a = np.argmax(ppp[k][l])
+                pi_states[k][l] += actions[a]
+
+    print(pi_states)
+    print(x)
